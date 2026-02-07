6c01c70ca22aab750b66d04668687a658b135e89
Xero-John <xerojohn7@gmail.com>
2026-02-07 14:13:17 +0800

    Test Commit

--- HelloWorld/src/main/java/com/xero/helloworld/MainForm.java
+++ HelloWorld/src/main/java/com/xero/helloworld/MainForm.java
@@ -16,6 +16,7 @@ public class MainForm extends javax.swing.JFrame {
     public MainForm() {
         initComponents();
         setLocationRelativeTo(null);
+        System.out.println("Hello World");
     }
 
     /**
